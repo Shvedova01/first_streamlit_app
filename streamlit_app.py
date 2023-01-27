e645347faa65a40c3f317712b99dff2c1f05719a
Shvedova01 <61554183+Shvedova01@users.noreply.github.com>
2023-01-26 19:56:35 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,34 +25,31 @@ streamlit.dataframe (fruits_to_show)
 
 
 
-# New section to display fruityvice api response
-streamlit.header("Fruityvice Fruit Advice!")
+#create a function
+def get_fruityvice_data (this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 
+#new section
+streamlit.header ('Fruityvice Fruit Advice!')
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  fruit_choice = streamlit.text_input ('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error ("Please select a fruit to get information.")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe (fruityvice_normalized)
-except URLError as e:
-  streamlit.error()
-    
+    back_from_function = get_fruityvice_data (fruit_choice)
+    streamlit.dataframe (back_from_function)
 
 
 #don't run below while we troubleshoot
-streamlit.stop()
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-
-
-streamlit.write ('Thanks for adding ', add_my_fruit')
-                 #this will not work corretly, but just do it
-my_cur.execute ("insert into fruit_load_list values ('from streamlit')")                 
+#streamlit.stop()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("select * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+#streamlit.header("The fruit load list contains:")
+#streamlit.dataframe(my_data_rows)
+#streamlit.write ('Thanks for adding ', add_my_fruit')
+#this will not work corretly, but just do it
+#my_cur.execute ("insert into fruit_load_list values ('from streamlit')")                 
